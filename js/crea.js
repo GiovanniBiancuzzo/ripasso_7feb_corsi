8da6588ccd37427dc43d39a0747ff9829c4b40ac
Giovanni Biancuzzo <39157315+GiovanniBiancuzzo@users.noreply.github.com>
2022-02-09 16:20:00 +0100

    Update crea.js

--- js/crea.js
+++ js/crea.js
@@ -12,11 +12,11 @@ class Corso {
     titolo;
     durata;
     argomenti;
-    constructor(_codice, _titolo, _durata,_argomenti) {
+    constructor(_codice, _titolo, _durata, _argomenti) {
         this.codice = _codice;//valori che arrivano al costruttore all'istanziazione
         this.titolo = _titolo;
         this.durata = _durata;
-        this.argomenti=_argomenti;
+        this.argomenti = _argomenti;
     }
 
     corsoCompleto() {
@@ -28,73 +28,73 @@ class Corso {
 $(() => {//logout
     let email = localStorage.getItem('email');//prendo l'email dal local storage
     let index = email.indexOf('@');//seleziono l'indice della chiocciola e
-    let utente = email.slice(0,index);//rimuovo la parte finale dell'email '@blabla.com'
+    let utente = email.slice(0, index);//rimuovo la parte finale dell'email '@blabla.com'
     $('header>button').text(email);// email in alto a destra al posto del login
     $('#benvenuto').text('Benvenuto ' + utente);
 
-    $('#convalida').on('click', function() {
+    $('#convalida').on('click', function () {
         const nomeCorso = $('#nomeCorso').val();
         const durataCorso = $('#durataCorso').val();
         const materia1 = $('#materia1').val();
         const materia2 = $('#materia2').val();
         const materia3 = $('#materia3').val();
         let convalida = true;
-        
+
         if (nomeCorso == '') {//validazione nome corso
-            nomeCorso.append('Inserisci un nome per il corso');
+            $('#nomeCorso').next().text('Inserisci un nome per il corso').css('color', 'red');
             convalida = false;
         }
         else {
-            $('#nomeCorso').append('');
+            $('#nomeCorso').next().text('');
         }
         $('#nomeCorso').val(nomeCorso);
 
 
         if (durataCorso <= 0) {//validazione durata corso
-            durataCorso.append('Inserisci un numero di settimane per il corso');
+            $('#durataCorso').next().text('Inserisci un numero di mesi per il corso').css('color', 'red');
             convalida = false;
         }
         else {
-            $('#durataCorso').append('');
+            $('#durataCorso').next().text('');
         }
         $('#durataCorso').val(durataCorso);
 
 
-        
+
         if (materia1 == '') {//validazione prima materia
-            materia1.append('Inserisci la prima materia');
+            $('#materia1').next().text('Inserisci la prima materia').css('color', 'red');
             convalida = false;
         }
         else {
-            $('#materia1').append('');
+            $('#materia1').next().text('');
         }
         $('#materia1').val(materia1);
-        
+
         if (materia2 == '') {//validazione seconda materia
-            materia2.append('Inserisci la seconda materia');
+            $('#materia2').next().text('Inserisci la seconda materia').css('color', 'red');
             convalida = false;
         }
         else {
-            $('#materia2').append('');
+            $('#materia2').next().text('');
         }
         $('#materia2').val(materia2);
 
         if (materia3 == '') {//validazione terza materia
-            materia3.append('Inserisci la terza materia');
+            $('#materia3').next().text('Inserisci la terza materia').css('color', 'red');
             convalida = false;
         }
         else {
-            $('#materia3').append('');
+            $('#materia3').next().text('');
         }
         $('#materia3').val(materia3);
 
         if (convalida == true) {
-            let sigla = (nomeCorso.slice(0,1)) + nomeCorso.slice(nomeCorso.length-1,nomeCorso.length) //creazione sigla a partire dal nome del corso, prima e ultima lettera del nome del corso
+            let sigla = (nomeCorso.slice(0, 1)) + nomeCorso.slice(nomeCorso.length - 1, nomeCorso.length) //creazione sigla a partire dal nome del corso, prima e ultima lettera del nome del corso
             sigla = sigla.toUpperCase();//sigla resa maiuscola
-/*inutile?*/let corso = new Corso (sigla, nomeCorso, durataCorso, `${materia1}, ${materia2}, ${materia3}`); //istanziazione della classe Corso con  i valori appena inseriti 
+/*inutile?*/let corso = new Corso(sigla, nomeCorso, durataCorso, `${materia1}, ${materia2}, ${materia3}`); //istanziazione della classe Corso con  i valori appena inseriti 
 
             $('#nuovoCorso').html(`Hai creato un corso ${corso.corsoCompleto()}<br>`);//stampa attraverso il metodo corsoCompleto del corso appena creato
-            
+
             localStorage.setItem('codiceCorso', sigla);
             localStorage.setItem('nomeCorso', nomeCorso);//riesco a salvarli nel localstorage, vorrei scriverli nell'elenco corsi
             localStorage.setItem('durataCorso', durataCorso);
@@ -106,7 +106,7 @@ $(() => {//logout
     })
 
 
-    $('#logout').on('click', function() {//pulsante logout che pulisce lo storage e riporta al login
+    $('#logout').on('click', function () {//pulsante logout che pulisce lo storage e riporta al login
         localStorage.clear();
         location.href = 'login.html';
     })
